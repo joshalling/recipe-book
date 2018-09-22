a69e45bc841763c7de82308f855447ef4d01320e
Josh Alling <joshralling@gmail.com>
2018-09-21 21:10:48 -0600

    add Header test and update tests to use MemoryRouter

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Header from './Header';
 import { MemoryRouter } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <MemoryRouter initialEntries={[ '/' ]}>
-      <App />
+      <Header />
     </MemoryRouter>,
     div
   );
